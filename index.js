d6434d88fc43e1ccb64541efade421cb281aa5a3
KaterynaSukhomlynova <ekaterinka705@ukr.net>
2022-07-26 04:05:52 +0300

    Updated all files

--- index.js
+++ index.js
@@ -23,46 +23,52 @@ function showTime(date) {
 let dateElement = document.querySelector("#date");
 dateElement.innerHTML = showTime(new Date());
 
-function degreesC(event) {
-  event.preventDefault();
-  let link = document.querySelector("#degrees");
-  link.innerHTML = "17";
-}
-let celciusLink = document.querySelector("#celcius");
-celciusLink.addEventListener("click", degreesC);
-
-function degreesF(event) {
-  event.preventDefault();
-  let link = document.querySelector("#degrees");
-  link.innerHTML = "66";
-}
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", degreesF);
-
 function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#degrees").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  temperature = response.data.main.temp;
+  document.querySelector("#city").innerHTML = response.data.name.toUpperCase();
+  document.querySelector("#degrees").innerHTML = Math.round(temperature);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  let iconElement = document.querySelector("#weather-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
+
+function displayDegreesC(event) {
+  event.preventDefault();
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let degreesC = temperature;
+  degrees.innerHTML = Math.round(degreesC);
+}
+let celciusLink = document.querySelector("#celcius");
+celciusLink.addEventListener("click", displayDegreesC);
+
+function dispayDegreesF(event) {
+  event.preventDefault();
+  celciusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let degreesF = (temperature * 9) / 5 + 32;
+  degrees.innerHTML = Math.round(degreesF);
+}
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", dispayDegreesF);
 
 function searchCity(city) {
   let apiKey = "6a8ad4e4612fcf3f4171168677e2bff3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-searchCity("Lima");
 
 function submitCity(event) {
   event.preventDefault();
   let city = document.querySelector("#search-input").value;
-  city = city.toUpperCase();
   searchCity(city);
 }
 let searchForm = document.querySelector("#search-form");
@@ -80,3 +86,5 @@ function getCurrentLocation(event) {
 }
 let currentLocationButton = document.querySelector("#current-location");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("LIMA");
