aa42a9f31e0594debf3f90486054579f27e4022f
KaterynaSukhomlynova <ekaterinka705@ukr.net>
2022-08-14 02:43:10 +0300

    Updated Forecast

--- index.js
+++ index.js
@@ -23,7 +23,59 @@ function showTime(date) {
 let dateElement = document.querySelector("#date");
 dateElement.innerHTML = showTime(new Date());
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row forecast-row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 7) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+              <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+              <img
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
+                  width="42"
+                />
+              <div class="weather-forecast-temperatures">
+                <span class="forecast-temperature-max" id="forecastTempMax"> ${Math.round(
+                  forecastDay.temp.max
+                )}° </span>
+                <span class="forecast-temperature-min"> ${Math.round(
+                  forecastDay.temp.min
+                )}° </span>
+              </div>
+            </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
+  document.querySelector("#uvi").innerHTML = Math.round(
+    response.data.current.uvi
+  );
+}
+
+function getForecast(coordinates) {
+  let apiKey = "6a8ad4e4612fcf3f4171168677e2bff3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
+  console.log(response.data);
   temperature = response.data.main.temp;
   document.querySelector("#city").innerHTML = response.data.name.toUpperCase();
   document.querySelector("#degrees").innerHTML = Math.round(temperature);
@@ -38,6 +90,8 @@ function displayWeatherCondition(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function displayDegreesC(event) {
@@ -57,6 +111,7 @@ function dispayDegreesF(event) {
   let degreesF = (temperature * 9) / 5 + 32;
   degrees.innerHTML = Math.round(degreesF);
 }
+
 let fahrenheitLink = document.querySelector("#fahrenheit");
 fahrenheitLink.addEventListener("click", dispayDegreesF);
 
